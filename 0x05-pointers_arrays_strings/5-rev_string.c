3f07e0ea8b938243d9e28b100435db848d564530
Hope Saviour <futurehopesaviour@gmail.com>
2022-04-20 14:55:36 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: string to be reverse
+ * Description: a function that reverses a string
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i = 0, x = 0, y;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (i > x)
+	{
+		y = s[i];
+		s[i--] = s[x];
+		s[x++] = y;
+	}
+}
