83b2a5838a162c66a4db31b639489a135be8ccfa
Hope Saviour <futurehopesaviour@gmail.com>
2022-04-20 15:44:16 -0700

    Optional task 1

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -21,7 +21,7 @@ int _atoi(char *s)
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			num = num * 10 + (s[i] - '0');
-			if (s[i + 1] < '0' || s[i + 1] > '9'9)
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 			{
 				break;
 			}
