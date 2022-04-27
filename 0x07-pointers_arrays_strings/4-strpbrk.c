4617047563cc6775f88a99dd98564466815aebcc
Hope Saviour <futurehopesaviour@gmail.com>
2022-04-27 11:53:56 -0700

    new

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,23 +1,25 @@
-#include <stdio.h>
-
-int main(void)
+#include "main.h"
+/**
+ * _strpbrk -  searches a string for any of a set of bytes
+ * @s:string
+ * @accept:string
+ * Return:char
+ */
+char *_strpbrk(char *s, char *accept)
 {
-  int n;
-  int a[5];
-  int *p;
+int i;
 
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-  p[5] = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+	if (*s == accept[i])
+	{
+		return (s);
+	}
+
+}
+s++;
+}
+return ('\0');
 }
