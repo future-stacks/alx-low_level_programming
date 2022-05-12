3237542e5e932ad4ede6f30ea83495e716fd7299
Hope Saviour <futurehopesaviour@gmail.com>
2022-05-12 02:12:17 -0700

    print program

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print file name
+ * Description: a program that prints the name of the file
+ * it was compiled from, followed by a line
+ * Return: void
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+		return (0);
+}
