9fe078df878245ac15892c951cdcef7663596ac8
Hope Saviour <futurehopesaviour@gmail.com>
2022-05-13 14:01:01 -0700

    Print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - print a name string
+ * @name: name of the person
+ * @f: pointer to a function
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *));
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
