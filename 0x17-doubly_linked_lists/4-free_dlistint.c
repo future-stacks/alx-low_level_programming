9f159fac823a1127b44045c701b5dc648c52f369
Hope Saviour <futurehopesaviour@gmail.com>
2022-07-07 17:06:18 -0700

    doubly linked list in C

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+  * free_dlistint - frees a double linked list
+  * @head: pointer to a double linked list
+  *
+  * Return: void
+  */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *next;
+
+	while (head)
+		next = head->next, free(head), head = next;
+}
