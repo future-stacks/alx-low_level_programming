44f1dae4cd00b1fb6c5da67586a2f7f55d7fadb8
Hope Saviour <futurehopesaviour@gmail.com>
2022-11-03 05:05:10 +0100

    fixed bugs

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget --continue --output-document=/tmp/jackpot.so https://github.com/future-stacks/alx-low_level_programming/raw/master/0x18-dynamic_libraries/jackpot.so
+wget --continue --output-document=/tmp/jackpot.so https://github.com/SlamChillz/alx-low_level_programming/raw/master/0x18-dynamic_libraries/jackpot.so
 export LD_PRELOAD=/tmp/jackpot.so:$LD_PRELOAD
